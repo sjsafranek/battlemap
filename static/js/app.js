1d12d88d5d5741603b8291bfaa292f4a69f458c7
Stefan Safranek <stefan@stefan>
2020-05-01 10:25:28 -0700

    check if canvas is blank

--- static/js/app.js
+++ static/js/app.js
@@ -315,7 +315,13 @@ App.prototype.onDrawLayer = function(event) {
         ctx.stroke();
 
         // TODO check if canvas is empty
-        console.log('isCanvasBlank', isCanvasBlank(event.canvas));
+        if (isCanvasBlank(event.canvas)) {
+            console.log("isCanvasBlank before splice", this.drawFeatures.length);
+            this.drawFeatures = this.drawFeatures.splice(i+1);
+            console.log("isCanvasBlank after splice", this.drawFeatures.length);
+            this.onDrawLayer(event);
+            return;
+        }
 
     }
 };
